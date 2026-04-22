28024734ebe60785cdc3ccffe4a755ed8918367b
BrendonLee23 <brendow_prime@hotmail.com>
2026-04-21 23:06:31 -0300

    chore: initial project setup

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/layout.tsx',
+    '!src/app/page.tsx',
+    '!src/app/tasks/page.tsx',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  clearMocks: true,
+}
+
+export default createJestConfig(config)
